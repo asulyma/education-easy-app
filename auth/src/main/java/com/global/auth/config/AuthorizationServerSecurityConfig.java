042edf6dca16149f9d4e24c40c34d26de809949a
Oleksandr Sulyma <Oleksandr_Sulyma@mckesson.com>
2020-10-03 10:38:55 +0300

    feature/031020: Cover AUTH module with IT tests; Upgrade dependencies

--- auth/src/main/java/com/global/auth/config/AuthorizationServerSecurityConfig.java
+++ auth/src/main/java/com/global/auth/config/AuthorizationServerSecurityConfig.java
@@ -1,10 +1,7 @@
 package com.global.auth.config;
 
-import com.global.auth.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.*;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,37 +9,40 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import com.global.auth.service.UserService;
+
+
 @Configuration
 public class AuthorizationServerSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    @Override
-    protected UserDetailsService userDetailsService() {
-        return userService;
-    }
+	@Override
+	protected UserDetailsService userDetailsService() {
+		return userService;
+	}
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService);
-    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userService);
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().ignoringAntMatchers("/oauth/token/");
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().ignoringAntMatchers("/oauth/token/");
+	}
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
-    }
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		super.configure(web);
+	}
 
-    @Lazy
-    @Override
-    @Bean(name = "authenticationManager")
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+	@Lazy
+	@Override
+	@Bean(name = "authenticationManager")
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
 }
--- auth/src/test/java/com/global/auth/IT/AuthApplicationIT.java
+++ auth/src/test/java/com/global/auth/IT/AuthApplicationIT.java
@@ -1,64 +1,129 @@
 package com.global.auth.IT;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.*;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.*;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = { AuthApplicationIT.Initializer.class })
 public class AuthApplicationIT {
 
-    @LocalServerPort
-    private int port;
+	private static final String IMAGE = "mdillon/postgis:10";
+	private static final String DB_NAME = "education_auth";
+	private static final String USERNAME = "education-auth";
+	private static final String PASSWORD = "education-auth";
 
-    @Test(expected = HttpClientErrorException.class)
-    public void authorizationRequestShouldFall() {
-        // PREDICATES
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(APPLICATION_JSON);
-        headers.set("Authorization", "Basic: wrongKey");
+	@ClassRule
+	public static PostgreSQLContainer<?> postgreSQL = new PostgreSQLContainer<>(IMAGE).withDatabaseName(DB_NAME)
+			.withUsername(USERNAME)
+			.withPassword(PASSWORD);
 
-        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+	@LocalServerPort
+	private int port;
 
-        // FUNCTIONALITY & TESTS
-        String url = getBaseUrl() + "token?grant_type=client_credentials";
-        new RestTemplate().exchange(url, HttpMethod.POST, entity, String.class);
-    }
+	@Test(expected = HttpClientErrorException.class)
+	public void authorizationRequestShouldFall() {
+		// PREDICATES
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Basic: wrongKey");
 
-    @Test
-    public void authorizationRequestShouldPassAndReturnToken() throws JSONException {
-        // PREDICATES
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(APPLICATION_JSON);
-        headers.set("Authorization", "Basic ZWR1Y2F0aW9uLXdlYi1jbGllbnQ6ZWR1Y2F0aW9uLXdlYi1jbGllbnQtc2VjcmV0");
+		HttpEntity<String> entity = new HttpEntity<>(null, headers);
 
-        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+		// FUNCTIONALITY & TESTS
+		String url = getBaseUrl() + "token?grant_type=client_credentials";
+		new RestTemplate().exchange(url, HttpMethod.POST, entity, String.class);
+	}
 
-        // FUNCTIONALITY
-        String url = getBaseUrl() + "token?grant_type=client_credentials";
-        ResponseEntity<String> response = new RestTemplate().exchange(url, HttpMethod.POST, entity, String.class);
+	@Test
+	public void shouldAuthorizeViaClient() throws JsonProcessingException {
+		// PREDICATES
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Basic ZWR1Y2F0aW9uLXdlYi1jbGllbnQ6ZWR1Y2F0aW9uLXdlYi1jbGllbnQtc2VjcmV0");
 
-        // TESTS
-        assertEquals(200, response.getStatusCodeValue());
-        assertNotNull(new JSONObject(response.getBody()).get("access_token"));
-    }
+		HttpEntity<String> entity = new HttpEntity<>(null, headers);
 
-    private String getBaseUrl() {
-        return "http://localhost:" + port + "/auth/oauth/";
-    }
+		// FUNCTIONALITY
+		String url = getBaseUrl() + "token?grant_type=client_credentials";
+		ResponseEntity<String> response = new RestTemplate().exchange(url, HttpMethod.POST, entity, String.class);
+
+		// TESTS
+		assertEquals(200, response.getStatusCodeValue());
+		ResponseObject data = new ObjectMapper().readValue(response.getBody(), ResponseObject.class);
+
+		assertEquals("standard-scope", data.getScope());
+		assertEquals("bearer", data.getToken_type());
+		assertNotNull(data.getAccess_token());
+	}
+
+	@Test
+	public void shouldAuthorizeViaUser() throws JsonProcessingException {
+		// PREDICATES
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Basic ZWR1Y2F0aW9uLXdlYi1jbGllbnQ6ZWR1Y2F0aW9uLXdlYi1jbGllbnQtc2VjcmV0");
+
+		HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+		// FUNCTIONALITY
+		String url = getBaseUrl() + "token?grant_type=password&username=john&password=john";
+		ResponseEntity<String> response = new RestTemplate().exchange(url, HttpMethod.POST, entity, String.class);
+
+		// TESTS
+		assertEquals(200, response.getStatusCodeValue());
+		ResponseObject data = new ObjectMapper().readValue(response.getBody(), ResponseObject.class);
+
+		assertEquals("standard-scope user-scope", data.getScope());
+		assertEquals("bearer", data.getToken_type());
+		assertNotNull(data.getAccess_token());
+	}
+
+	private String getBaseUrl() {
+		return "http://localhost:" + port + "/auth/oauth/";
+	}
+
+	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+		@Override
+		public void initialize(@NotNull ConfigurableApplicationContext context) {
+			String[] properties = new String[4];
+			properties[0] = "spring.datasource.url=" + postgreSQL.getJdbcUrl();
+			properties[1] = "spring.datasource.username=" + postgreSQL.getUsername();
+			properties[2] = "spring.datasource.password=" + postgreSQL.getPassword();
+			properties[3] = "spring.jpa.hibernate.ddl-auto=none";
+
+			TestPropertyValues.of(properties).applyTo(context);
+		}
+	}
+
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	private static final class ResponseObject {
+		private String access_token;
+		private String refresh_token;
+		private String token_type;
+		private String expires_in;
+		private String scope;
+	}
 
 }
